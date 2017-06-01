1c1bd1ac21fb7702686d5c567926afe1963706b9
ttaix <tansu@aibrite.com>
2017-06-01 16:49:43 +0300

    initial commit

---
+++ lib/definitions/provider.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="es6-promise" />
+import { Query, QueryResult, GetDashboardResult, DashboardUpdateModel, DashletModel, DashletUpdateModel, DashboardModel, CreateResult } from 'jdash-core';
+export interface ISearchDashboard {
+    appid?: string | Array<string>;
+    user?: string | Array<string>;
+    shareWith?: string | Array<string>;
+}
+export interface ISearchDashlet {
+    user?: string | Array<string>;
+    dashboardId?: string;
+}
+export interface IDBProvider {
+    getDashboard?(appid: string, id: string): Promise<GetDashboardResult>;
+    searchDashboards(search: ISearchDashboard, query?: Query): Promise<QueryResult<DashboardModel>>;
+    createDashboard(appid: string, model: DashboardModel): Promise<CreateResult>;
+    deleteDashboard(appid: string, id: string): Promise<any>;
+    updateDashboard(appid: string, id: string, updateValues: DashboardUpdateModel): Promise<any>;
+    createDashlet(model: DashletModel): Promise<CreateResult>;
+    searchDashlets(search: ISearchDashlet): Promise<Array<DashletModel>>;
+    deleteDashlet(id: string): Promise<any>;
+    updateDashlet(id: string, updateValues: DashletUpdateModel): Promise<any>;
+}
